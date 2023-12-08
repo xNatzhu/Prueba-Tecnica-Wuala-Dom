c7b6a317d1a1b42f14ab5a316af92c1b8a2ed7ae
Agustin Saravia <87948201+xNatzhu@users.noreply.github.com>
2023-12-08 15:11:02 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,14 +2,32 @@ const card = document.getElementById("card");
 
 City();
 
-
 card.addEventListener("click", () => {
   City();
 });
 
 function City() {
-  const listCity = ["Buenos Aires", "Cordoba", "Cali", "Santiago", "Damasco","Tokio", "Japon", "Paris", "Roma", "Nueva York", "Barcelona", "California", "Amsterdam", "Manchester","Madrid", "Berlin", "Moscu", "Lyon"]
-  const randomCity = Math.floor(Math.random()*listCity.length)
+  const listCity = [
+    "Buenos Aires",
+    "Cordoba",
+    "Cali",
+    "Santiago",
+    "Damasco",
+    "Tokio",
+    "Japon",
+    "Paris",
+    "Roma",
+    "Nueva York",
+    "Barcelona",
+    "California",
+    "Amsterdam",
+    "Manchester",
+    "Madrid",
+    "Berlin",
+    "Moscu",
+    "Lyon",
+  ];
+  const randomCity = Math.floor(Math.random() * listCity.length);
   const URLAPICITY = `https://api.openweathermap.org/data/2.5/forecast?q=${listCity[randomCity]}&APPID=6752644c4b10d307e40b484055d4f5a5&units=metric`;
 
   fetch(URLAPICITY)
@@ -23,29 +41,32 @@ function City() {
     });
 }
 
-
 function CardElement(data) {
-    const iconCity = data.list[0].weather[0].icon;
-    const { temp_max, temp_min, humidity, pressure } = data.list[0].main;
-    console.log(data.list[0].sys.pod);
+  const iconCity = data.list[0].weather[0].icon;
+  const { temp_max, temp_min, humidity, pressure } = data.list[0].main;
+  console.log(data.list[0].sys.pod);
   const cardContainer = `
     <div class="card-header">
-    <img src="${data.list[0].sys.pod === "n" ? "./img/background/dark.jpg" : "./img/background/light.jpg"}" alt="rover" />
+    <img src="${
+      data.list[0].sys.pod === "n"
+        ? "./img/background/dark.jpg"
+        : "./img/background/light.jpg"
+    }" alt="rover" />
 </div>
 <div class="card-clime">
     <img id="mainIcon" src="./img/animated/${iconCity}.svg" alt="" srcset="">
-    <h2>${data.list[0].main.temp}°</h2>
+    <h2>${Math.round(data.list[0].main.temp)}°</h2>
 </div>
 <div class="card-body">
-    <span class="tag ${data.list[0].sys.pod === "n" ? "tag-dark" : "tag-day"}">${data.city.name}</span>
+    <span class="tag ${
+      data.list[0].sys.pod === "n" ? "tag-dark" : "tag-day"
+    }">${data.city.name}</span>
     <h4 id="mainTitle">¿Cómo está el clima hoy en ${data.city.name}?</h4>
     <p id="descriptionCity">
         El clima se encuentra con una temperatura de ${temp_min}° la min, y la max con ${temp_max}°. Tiene una humedad de ${humidity}% y una presión de ${pressure} hPa.
     </p>
 </div>
     
-    `
-    card.innerHTML = cardContainer
-    ;
-  
+    `;
+  card.innerHTML = cardContainer;
 }
